90b8b277add14d808f5feb75553672dc816a1c40
Lucky Cat <2803404074@qq.com>
2020-07-13 17:35:45 +0800

    启动、广告

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,80 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'navigator/tab_navigator.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  //初始化数据后跳转广告页
+  Future.delayed(Duration(seconds: 2), () {
+    runApp(MyApp());
+  });
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppPageState createState() => _MyAppPageState();
+}
+
+class _MyAppPageState extends State<MyApp> {
+  var seconds = 5;
+  Timer countdownTimer;
+
+  bool isMissingAdv = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    countdownTimer = new Timer.periodic(new Duration(seconds: 1), (timer) {
+      setState(() {
+        if (seconds > 0) {
+          seconds--;
+        } else {
+          isMissingAdv = true;
+          countdownTimer.cancel();
+          countdownTimer = null;
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: TabNavigator(),
+      home: Stack(
+        children: <Widget>[
+          TabNavigator(),
+          Offstage(
+            offstage: isMissingAdv,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.white,
+                      width: double.infinity,
+                      child: Image.network(
+                        'http://img.pconline.com.cn/images/upload/upc/tx/itbbs/1609/30/c48/27767814_1475244403695_1024x1024.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+                Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  color: Colors.white,
+                  child: Text('$seconds'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
